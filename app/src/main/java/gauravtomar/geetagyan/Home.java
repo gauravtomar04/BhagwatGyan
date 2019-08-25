20e6edb197b08e16ec3b057ad03e3ae1b4fec69c
Gaurav Tomar <gauravtomar9210@gmail.com>
2019-08-25 12:12:26 +0530

    listview added with firebase

--- app/src/main/java/gauravtomar/geetagyan/Home.java
+++ app/src/main/java/gauravtomar/geetagyan/Home.java
@@ -3,13 +3,8 @@ package gauravtomar.geetagyan;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,7 +12,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;///
+import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
 
@@ -72,8 +67,7 @@ public class Home extends AppCompatActivity {
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
+            public void onCancelled(DatabaseError databaseError) {
             }
         } );
 
